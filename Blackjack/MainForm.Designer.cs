4b40dab730587aeeb5bb288dfc8e57fea0be2ff6
Roman <rwendelboe@yahoo.com>
2015-11-16 01:09:44 -0700

    Made the Bet and Pot text boxes unchnagable by the user

--- Blackjack/MainForm.Designer.cs
+++ Blackjack/MainForm.Designer.cs
@@ -54,7 +54,6 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-
             // 
             // mainMenu1
             // 
@@ -277,6 +276,7 @@
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.85F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(23, 700);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(294, 60);
             this.richTextBox1.TabIndex = 11;
             this.richTextBox1.Text = "Bet: ";
@@ -290,9 +290,10 @@
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.85F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.Location = new System.Drawing.Point(23, 783);
             this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
             this.richTextBox2.Size = new System.Drawing.Size(294, 60);
             this.richTextBox2.TabIndex = 12;
-            this.richTextBox2.Text = "Pot:";
+            this.richTextBox2.Text = "Pot: $1000";
             this.richTextBox2.Visible = false;
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
