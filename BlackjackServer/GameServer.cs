ad405babce7a60bab3b45e4f99f9a3bcb2960c8c
ethanbenowitz <ethanb1994@gmail.com>
2015-12-01 12:43:06 -0700

    Added GameServer.cs

---
+++ BlackjackServer/GameServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackServer
+{
+    class GameServer
+    {
+        private EasyNetwork.Server server = new EasyNetwork.Server("tcp://*:3000");
+        private bool isRunning = true;
+
+        public void start()
+        {
+            server.DataReceived += Server_DataReceived;
+            server.Start();
+
+            while (isRunning)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            server.Stop();
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+
+        private void Server_DataReceived(object receivedObject, Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlackjackServer/Program.cs
+++ BlackjackServer/Program.cs
@@ -10,7 +10,16 @@ namespace BlackjackServer
     {
         static void Main(string[] args)
         {
+            GameServer server = new GameServer();
 
+            Console.CancelKeyPress += delegate
+            {
+                Console.WriteLine("Ctrl+C caught, stopping server...");
+                server.Stop();
+            };
+
+            
+            server.start();
         }
     }
 }
