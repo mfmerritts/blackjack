1996ea0928b95273ada822c89c1b1424dce25260
rwendelboe <rwendelboe@gmail.com>
2015-12-02 18:59:25 -0700

    Changes made to labels

--- Blackjack/MainForm.Designer.cs
+++ Blackjack/MainForm.Designer.cs
@@ -409,7 +409,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(396, 405);
+            this.label2.Location = new System.Drawing.Point(396, 403);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 31);
             this.label2.TabIndex = 19;
